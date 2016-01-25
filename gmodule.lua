a25e293345db8b23acb1411e381b392bcc819586
Malcolm Reynolds <mareynolds@google.com>
2016-01-25 14:48:17 +0000

    Don't bother filling a Tensor with zero right before we copy into it

--- gmodule.lua
+++ gmodule.lua
@@ -11,8 +11,8 @@ local function getTotalGradOutput(node)
       node.data.gradOutputBuffer = node.data.gradOutputBuffer or nesting.cloneNested(gradOutput[1])
       local gobuff = node.data.gradOutputBuffer
       nesting.resizeNestedAs(gobuff, gradOutput[1])
-      nesting.fillNested(gobuff, 0)
-      for i=1,#gradOutput do
+      nesting.copyNested(gobuff, gradOutput[1])
+      for i=2,#gradOutput do
          nesting.addNestedTo(gobuff, gradOutput[i])
       end
       gradOutput = gobuff
